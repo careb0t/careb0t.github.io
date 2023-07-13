ad806c60b51fb5e78a5929cba02032ac785fb5cb
Alex Bond <obadere@gmail.com>
2023-07-13 16:04:27 -0700

    portfolio update

---
+++ src/components/data/social.js
@@ -0,0 +1,13 @@
+export const SocialData=[
+    
+    {
+        platform:"Github",
+        icon: require("../../assets/icons/github.png"),
+        link:"https://github.com/careb0t",
+    },
+    {
+        platform:"Linkedin",
+        icon: require("../../assets/icons/linkedin.png"),
+        link:"https://www.linkedin.com/in/gage-bond-501866183/",
+    },
+];
---
+++ src/components/data/work.js
@@ -0,0 +1,26 @@
+export const WorkData = [
+    {
+        company: "FastVisa" ,
+        designation: "Frontend Web Developer",
+        dateJoining: "2020",
+        dateEnd: "2021",
+        companyLogo:"https://d33wubrfki0l68.cloudfront.net/6c34fc29c54b083241d7c77e31e0c78fc43cc013/b561a/logo.png",
+        work:"Worked with inhouse designers and clients to create a unique web service tailored to their specific needs.",
+    },
+    {
+        company: "Scribbler" ,
+        designation: "Web Developer",
+        dateJoining: "2018",
+        dateEnd: "2019",
+        companyLogo:"https://www.goscribbler.com/wp-content/themes/cream-blog-child/assets/dist/images/typewriter-logo.png",
+        work:"Developed interface for client-facing e-Commerce shop and created a blog/SEO generation site for inhouse writers to publish with WordPress."
+    },
+    {
+        company: "Freelance Work" ,
+        designation: "Web Developer & Designer",
+        dateJoining: "2017",
+        dateEnd: "Present",
+        companyLogo:"https://logo.com/image-cdn/images/kts928pd/production/c43174cfbf41d5b0d5b19c709000a70b29294e4b-414x410.png?w=1080&q=72",
+        work:"Worked with clients to create memorable web presences to increase their recognition and engagement online."
+    },
+]
